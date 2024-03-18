f4de4886359a9b83d182c58b3aedcad2defad252
hyobin <hbaloha@naver.com>
2024-03-18 19:49:50 +0900

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
+  useEffect(() => {
+    console.log('hi :)');
+    return () => console.log('bye :(');
+  }, []);
+  useEffect(function () {
+    console.log('hi :)');
+    return function () {
+      console.log('bye :(');
+    };
+  }, []);
+  return <h1>Hello!</h1>;
+}
 
-  useEffect(() => {
-    console.log('I run only once.');
-  }, []); // [] 빈 array는 지켜볼 게 없으니까 처음 한 번만 실행
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]); //keyword를 지켜보니까 변화할 때마다 코드가 항상 실행
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log('I run when keyword and counter change');
-  }, [keyword, counter]);
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
