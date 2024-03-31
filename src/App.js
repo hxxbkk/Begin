4d37fb1a282318129661f623c34b2e0278ca628f
hyobin <hbaloha@naver.com>
2024-03-31 18:26:19 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import { useState, useEffect } from 'react';
-import Movie from './components/Movie';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="Movie" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import Movie from '../components/Movie';
-
+import { useState, useEffect } from 'react';
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
